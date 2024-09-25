5af95d02ed4ab8d565f51a4bbd04b13b84984419
Shirish Shakya <shirish.shakya5@gmail.com>
2024-09-25 19:01:58 -0400

    analytics added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,6 +3,7 @@ import { AppProvider } from '@shopify/polaris';
 import '@shopify/polaris/build/esm/styles.css';
 import translations from '@shopify/polaris/locales/en.json';
 import "@/styles/styles.scss"
+import { Analytics } from "@vercel/analytics/react"
 
 export default function App({ Component, pageProps }) {
   return <AppProvider i18n={translations}>
