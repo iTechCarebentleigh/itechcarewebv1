8361a11ca671d397919b6aa8b241e6c7e547ca05
Shirish Shakya <shirish.shakya5@gmail.com>
2024-10-20 03:02:01 -0400

    Script added

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,5 +1,6 @@
 import { Html, Head, Main, NextScript } from "next/document";
 import { Header } from "@/components";
+import Script from "next/script";
 
 export default function Document() {
   return (
