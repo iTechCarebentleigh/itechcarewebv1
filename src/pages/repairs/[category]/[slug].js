85c584325d18812dbdef1b996cf331802113dce3
Shirish Shakya <shirish.shakya5@gmail.com>
2024-10-21 15:57:27 -0400

    trying server props and paths

--- src/pages/repairs/[category]/[slug].js
+++ src/pages/repairs/[category]/[slug].js
@@ -1,18 +1,16 @@
 import { useState, useEffect } from "react";
 import { Inter } from "next/font/google";
 import { Usersform, Header, Countdown, Footer } from "@/components";
-import Head from "next/head";
 import { Button } from '@shopify/polaris';
 import Link from "next/link";
-import { client } from '../../../../sanity/lib/client'; // Import the sanity client
-import { useRouter } from "next/router";
+import { client } from '../../../../sanity/lib/client';
 import { urlForImage } from "../../../../sanity/lib/image";
 import Image from "next/image";
-import { NextSeo } from 'next-seo';
+import { NextSeo } from 'next-seo'; // Import NextSeo
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function RepairPage({ repair, category, urlSegments }) {
+export default function RepairPage({ repair, category }) {
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
@@ -21,78 +19,81 @@ export default function RepairPage({ repair, category, urlSegments }) {
     }
   }, [repair]);
 
-  console.log("Repair in Component:", repair);
+  console.log("Repair in Component:", repair, category);
 
   if (isLoading) {
     return <p>Loading...</p>; // Or any loading indicator
   }
 
+  const seo = {
+    title: repair?.hero?.heroTitle || 'Default Title',
+    description:  'Default Description',
+    openGraph: {
+      url: 'https://www.itechcare.com.au',
+      title: repair?.hero?.heroTitle || 'Default Title',
+      description: repair?.hero?.heroDescription || 'Default Description',
+      images: [
+        {
+          url: repair.coverImage ? urlForImage(repair.coverImage).url() : '/cover-image.png',
+          width: 1200,
+          height: 630,
+          alt: 'Cover Image',
+        },
+      ],
+      site_name: 'iTech Care',
+    },
+    twitter: {
+      handle: '@itechcare',
+      site: '@itechcare',
+      cardType: 'summary_large_image',
+    },
+  };
+
   return (
     <>
-    <NextSeo
-  description={repair.metaDescription }
-  title={`${category.charAt(0).toUpperCase() + category.slice(1)} - ${repair.repairItemTitle}`}
-  openGraph={{
-    url: 'https://www.itechcare.com.au',
-    title: repair.repairItemTitle,
-    description: repair.metaDescription,
-    images: [
-      {
-        url: urlForImage(repair.coverImage).url(), // Use appropriate image URL here
-        width: 1200,
-        height: 630,
-        alt: 'Repairs Cover Image',
-      },
-    ],
-  }}
-  
-/>
+            <NextSeo {...seo} />
+
 
       <main>
         <section className="bg-white h-fit lg:h-[540px]">
-        <div className="h-full w-full    lg:px-0 flex flex-col justify-center relative">
-    {/* Background Image for Slide 1 */}
-    <Image
-    width={10000}
-    alt={repair.coverImage ? repair.coverImage.alt:''}
-    height={10000}
-    src={repair.coverImage ? urlForImage(repair.coverImage).url() : ''}
-    className="w-full h-full absolute object-cover "
-      style={{ zIndex: 0, top: 0, left: 0 }}
-    />
+          <div className="h-full w-full lg:px-0 flex flex-col justify-center relative">
+            {/* Background Image for Slide 1 */}
+            <Image
+              width={10000}
+              alt={repair.coverImage ? repair.coverImage.alt : ''}
+              height={10000}
+              src={repair.coverImage ? urlForImage(repair.coverImage).url() : ''}
+              className="w-full h-full absolute object-cover"
+              style={{ zIndex: 0, top: 0, left: 0 }}
+            />
 
-    {/* Gradient Layer */}
-    <div
-      className="bg-gradient-to-r from-[rgba(24,24,27,1)] to-[rgba(24,24,27,0.8)] lg:to-[rgba(24,24,27,0.2)]  absolute h-full w-full top-0"
-      style={{ zIndex: 1 }}
-    ></div>
+            {/* Gradient Layer */}
+            <div
+              className="bg-gradient-to-r from-[rgba(24,24,27,1)] to-[rgba(24,24,27,0.8)] lg:to-[rgba(24,24,27,0.2)] absolute h-full w-full top-0"
+              style={{ zIndex: 1 }}
+            ></div>
 
-    {/* Content Layer (on top of the gradient) */}
-    <div
-      className="relative container mx-auto md:max-w-2xl px-4 lg:px-0 py-32 lg:max-w-3xl xl:max-w-6xl flex flex-col gap-4"
-      style={{ zIndex: 2 }}
-    >
-      <div className='flex flex-col gap-4  w-full lg:w-2/4'>
-      <div className="w-16 bg-zinc-800 h-16 rounded-full flex items-center jusitfy-center">
-      <div className="flex items-center jusitfy-center mx-auto transform-scale(2)" dangerouslySetInnerHTML={{ __html: repair.icon.svg }} />
-
-        
-      </div>
-        <h1 className="text-3xl lg:text-5xl text-white">
-          {repair.hero.heroTitle}
-         
-        </h1>
-        <p className='text-lg text-zinc-300 font-normal w-full '>
-        {repair.hero.heroDescription}
-
-        </p>
-      </div>
-      <Link href={'/bookrepair'}>
-       <Button >Book Repair Now</Button>
-  
-</Link> 
-    </div>
-  </div>
+            {/* Content Layer (on top of the gradient) */}
+            <div
+              className="relative container mx-auto md:max-w-2xl px-4 lg:px-0 py-32 lg:max-w-3xl xl:max-w-6xl flex flex-col gap-4"
+              style={{ zIndex: 2 }}
+            >
+              <div className='flex flex-col gap-4 w-full lg:w-2/4'>
+                <div className="w-16 bg-zinc-800 h-16 rounded-full flex items-center justify-center">
+                  <div className="flex items-center justify-center mx-auto" dangerouslySetInnerHTML={{ __html: repair.icon.svg }} />
+                </div>
+                <h1 className="text-3xl lg:text-5xl text-white">
+                  {repair.hero.heroTitle}
+                </h1>
+                <p className='text-lg text-zinc-300 font-normal w-full'>
+                  {repair.hero.heroDescription}
+                </p>
+              </div>
+              <Link href={'/bookrepair'}>
+                <Button>Book Repair Now</Button>
+              </Link>
+            </div>
+          </div>
         </section>
         <Footer />
       </main>
@@ -100,43 +101,7 @@ export default function RepairPage({ repair, category, urlSegments }) {
   );
 }
 
-
-export async function getStaticPaths() {
-  try {
-    // Fetch all repairs to get unique categories and slugs
-    const queryPaths = `*[_type == "repairs"]{
-      categoryName,
-      repairs[] {
-        slug {
-          current
-        }
-      }
-    }`;
-
-    const fetchedPaths = await client.fetch(queryPaths);
-    
-    const paths = [];
-    fetchedPaths.forEach(category => {
-      const categoryName = category.categoryName;
-      category.repairs.forEach(repair => {
-        paths.push({
-          params: { category: categoryName, slug: repair.slug.current },
-        });
-      });
-    });
-
-    console.log("Generated Paths:", paths); // Log the generated paths
-
-    return {
-      paths,
-      fallback: 'blocking', // or true/false depending on your requirement
-    };
-  } catch (err) {
-    console.error("Error fetching paths:", err);
-    return { paths: [], fallback: 'blocking' };
-  }
-}
-export async function getStaticProps(context) {
+export async function getServerSideProps(context) {
   const { params } = context; // { category, slug }
 
   try {
@@ -158,7 +123,7 @@ export async function getStaticProps(context) {
         },
         _key,
         categoryName,
-        repairs[] 
+        repairs[]
       }
     }`;
 
@@ -186,15 +151,20 @@ export async function getStaticProps(context) {
     // Find the specific repair that matches the slug
     const repair = repairsArray.find(repair => repair.slug.current === slug);
 
+    if (!repair) {
+      return {
+        notFound: true,
+      };
+    }
+
     return {
       props: {
         repair: repair || null, // Ensure repair is null if not found
-        category:category || null
+        category: category || null
       },
-      revalidate: 60, // Re-generate the page every 60 seconds if a request comes in
     };
   } catch (err) {
     console.error("Error fetching repair data:", err);
-    return { props: { repair: null ,category : null} };
+    return { props: { repair: null, category: null } };
   }
 }
