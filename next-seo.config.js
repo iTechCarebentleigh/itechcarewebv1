eeeab60332242a3a634c3805488bcfeac714fdd4
Shirish Shakya <shirish.shakya5@gmail.com>
2025-01-29 21:04:41 -0500

    title changed again

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,10 +1,10 @@
 const SEO = {
-  title: 'iTech Care - Top Repairs, Quality Accessories in Bentleigh, Australia.',
+  title: 'iTech Care - Phone and Laptop Repairs in Bentleigh, Australia.',
   description: "At iTech Care in Bentleigh, Australia, we provide expert mobile phone repair services, including fixing cracked screens, water damage, charging issues, and more. Whether you need a quick screen replacement or a complex repair, our skilled technicians are here to help. Visit us for fast and reliable service!",
     openGraph: {
       type: 'website',
       url: 'https://www.itechcare.com.au',
-      title: 'iTech Care - Top Repairs, Quality Accessories in Bentleigh, Australia.',
+      title: 'iTech Care - Phone and Laptop Repairs in Bentleigh, Australia.',
       description: 'At iTech Care in Bentleigh, Australia, we provide expert mobile phone repair services, including fixing cracked screens, water damage, charging issues, and more. Whether you need a quick screen replacement or a complex repair, our skilled technicians are here to help. Visit us for fast and reliable service!',
       images: [
         {
